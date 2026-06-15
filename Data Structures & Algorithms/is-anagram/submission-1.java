c64d4d0a7192304f15d784bcc55414d14cb5bcab
Shivendra Yadav <146006699+146shiv@users.noreply.github.com>
2026-06-15 14:59:13 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        // char [] sArr = s.toCharArray();
+        // char [] tArr = t.toCharArray();
+
+        // Arrays.sort(sArr);
+        // Arrays.sort(tArr);
+
+        // for(int i = 0; i<sArr.length; i++){
+        //     if(sArr[i] != tArr[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        HashMap<Character,Integer> map1  = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+
+        for(int i = 0; i<s.length(); i++){
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0) +1);
+
+            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        if(map1.equals(map2)){
+            return true;
+        }
+        return false;
+
+    }
+}
